ce2819da1b13199f0785f2c43f7e5b925d7e70cf
Crowdbotics <engineering@crowdbotics.com>
2022-10-28 10:57:33 +0000

    Initial commit.

---
+++ backend/previtest_dev_41393/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for previtest_dev_41393 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'previtest_dev_41393.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "previtest_dev_41393",
+  url: "https://previtest_dev_41393.botics.co",
+  api: "https://previtest_dev_41393.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
